6c6e5650b47aba56db27d6f0e0cedbe9ee3d3615
faaiz <email>
2024-03-23 18:19:41 +0500

    redux tool kit

--- lms/src/Dashboard/Dashboard.jsx
+++ lms/src/Dashboard/Dashboard.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Sidebar from "../components/Sidebar/Sidebar"
-
+import { useDispatch,useSelecter } from 'react-redux'
 import "./style.css"
+import { getUser } from '../features/user'
 
 const Dashboard = () => {
+  const dispatch=useDispatch()
   return (
     <>
     <Sidebar/>
+    <button onClick={()=>dispatch(getUser())}></button>
     </>
 
   
---
+++ lms/src/features/user.js
@@ -0,0 +1,33 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const getUser = createAsyncThunk("getuser", async () => {
+  const response = await axios.get("https://api.github.com/users");
+  console.log("state",response.data.login)
+  return response;
+});
+getUser()
+export const UserSlice = createSlice({
+  name: "UserSlice",
+  initialState: {
+    user: [],
+    loading: false,
+    error: null,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getUser.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(getUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.user = action.payload;
+      })
+      .addCase(getUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default UserSlice.reducer;
